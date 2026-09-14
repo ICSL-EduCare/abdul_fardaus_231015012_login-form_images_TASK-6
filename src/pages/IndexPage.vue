785940e4d1095b892df00ca81b2941657ca0ba78
Abdul Fardaus <fardausjaman0@gmail.com>
2026-09-14 16:40:07 +0600

    Add login form

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,31 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <q-card class="flex flex-center my-card">
+    <div class="q-pa-md" style="max-width: 600px">
+      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+        <q-input
+          filled
+          v-model="name"
+          label="Username *"
+          lazy-rules
+          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+        />
+
+        <q-input
+          filled
+          type="password"
+          v-model="password"
+          label="Password *"
+          lazy-rules
+          :rules="[
+            (val) => (val !== null && val !== '') || 'Please type your age',
+            (val) => (val > 0 && val < 100) || 'Please type a real age',
+          ]"
+        />
+
+        <div>
+          <q-btn label="Submit" type="submit" color="primary" />
+        </div>
+      </q-form>
+    </div>
+  </q-card>
 </template>
